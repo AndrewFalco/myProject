933eea9f8ca308ada0d657e8075faf7c3c78b519
Andrew <am.sokolov93@yandex.ru>
2023-05-04 19:07:32 +0300

    bug?

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPageHeader/ArticleDetailsPageHeader.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPageHeader/ArticleDetailsPageHeader.tsx
@@ -3,8 +3,8 @@ import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { AppLink, Button, HStack } from '@/shared/ui';
 import { getArticleDetailsData } from '@/entities/Article';
-import { getCanEditArticle } from '../../model/selectors/article';
 import { getRoutArticleEdit, getRoutArticles } from '@/shared/consts/routes';
+import { getCanEditArticle } from '../../model/selectors/article';
 
 export const ArticleDetailsPageHeader = memo(() => {
     const { t } = useTranslation('article-details');
