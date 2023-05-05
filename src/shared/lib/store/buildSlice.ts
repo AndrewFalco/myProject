ed9a5e29e51fec41c303b46efe8247ebf88b41c0
Andrew <am.sokolov93@yandex.ru>
2023-05-05 12:09:30 +0300

    add hooks build selector and build slice

---
+++ src/shared/lib/store/buildSlice.ts
@@ -0,0 +1,26 @@
+import { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+import { CreateSliceOptions, bindActionCreators, createSlice } from '@reduxjs/toolkit';
+import { SliceCaseReducers } from '@reduxjs/toolkit/dist';
+
+// TODO: Переписать все на новые хуки
+
+export function buildSlice<
+        State,
+        CaseReducers extends SliceCaseReducers<State>,
+        Name extends string = string
+    >(options: CreateSliceOptions<State, CaseReducers, Name>) {
+    const slice = createSlice(options);
+
+    const useActions = (): typeof slice.actions => {
+        const dispatch = useDispatch();
+
+        // @ts-ignore
+        return useMemo(() => bindActionCreators(slice.actions, dispatch), [dispatch]);
+    };
+
+    return {
+        useActions,
+        ...slice,
+    };
+}
